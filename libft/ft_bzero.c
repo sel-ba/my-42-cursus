36d2942b0c8507072ddee929110309387af30988
Souhaib El Bakouri <souhaib_elbakouri@um5.ac.ma>
2024-10-27 17:04:31 +0100

    Update ft_bzero.c

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: sel-bako <sel-bako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:59:44 by sel-bako          #+#    #+#             */
-/*   Updated: 2024/10/27 17:02:27 by sel-bako         ###   ########.fr       */
+/*   Updated: 2024/10/27 17:03:58 by sel-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_bzero(void *s, size_t n)
 
 // int main()
 // {
-//printf("sel-bako");
+//	printf("sel-bako");
 // 	char s[20] = "sohaybbbbb";
 // 	ft_bzero(s+3,2);
 // 	printf("%s\n",s);
