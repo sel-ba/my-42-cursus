bce559f3043d93044bd2b668f1040dbbb3f2d66d
Souhaib El Bakouri <souhaib_elbakouri@um5.ac.ma>
2024-10-27 18:36:51 +0100

    Creating new functions

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,20 +6,31 @@
 /*   By: sel-bako <sel-bako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 21:06:35 by sel-bako          #+#    #+#             */
-/*   Updated: 2024/10/25 21:10:00 by sel-bako         ###   ########.fr       */
+/*   Updated: 2024/10/27 18:24:29 by sel-bako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *str1, const char *str2, size_t count)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 
 	i = 0;
+	if (n == 0)
+        return (0);
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
-	{
 		i++;
-	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 	char str1[] = "Hello, World!";
+// 	char str2[] = "Hello, World!";
+// 	size_t n = 5;
+// 	printf("The result is: %d\n", ft_strncmp(str1, str2, n));
+// 	return 0;
+// }
